1cc12c5eb4beff64f0035dd3806f46f6eec143c6
jandales <jesusandales021993@gmail.com>
2022-08-22 23:02:34 +0800

    fixed some bugs

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -13,7 +13,7 @@
         <nav class="hidden fixed w-full  min-h-screen top-0 right-0 z-10 backdrop-blur-md lg:relative lg:min-h-0 lg:w-auto lg:block" :class="{'!block' : isMenuOpen}">             
                 
                
-               <transition-group name="list" tag="ul" appear=""  class="w-4/5  md:w-1/2 absolute flex  flex-col items-center gap-8  right-0 top-0  min-h-screen  pl-0 list-style-none lg:relative lg:flex-row lg:min-h-0 lg:gap-4  ">
+               <transition-group name="list" tag="ul" appear=""  class="w-4/5 bg-dark   md:w-1/2 absolute flex  flex-col items-center gap-8  right-0 top-0  min-h-screen  pl-0 list-style-none lg:relative lg:flex-row lg:min-h-0 lg:gap-4 lg:bg-transparent ">
                     <font-awesome-icon @click="toggleMenu()" icon="fa-solid fa-xmark" class="p-8 pb-0 text-[3rem] self-end lg:hidden" />
                      <li @click="scrollToElement('home')" :key="'about'">
                         <a class="nav-item">
@@ -49,14 +49,15 @@ import { ref } from 'vue';
 const isMenuOpen = ref(false);
 
 const toggleMenu  = () =>  {
-    console.log('dadad');
+   
     isMenuOpen.value = isMenuOpen.value == true ? false : true;
-    console.log(isMenuOpen.value)
+    
 }
 
 const  scrollToElement = (refName)  => {    
       const el = document.getElementById(refName);
        el.scrollIntoView({behavior : "smooth"})
+       isMenuOpen.value =  false;
 }
 
 
