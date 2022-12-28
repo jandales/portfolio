5cf8a166e6140bd9d114f0e5a41cddd1df7de779
jesusandales021993@gmail.com <jesusandales021993@gmail.com>
2022-12-29 00:44:36 +0800

    update experiences

--- src/components/Base/about/Experience.vue
+++ src/components/Base/about/Experience.vue
@@ -2,20 +2,32 @@
     <div v-scrollanimation class="mb-4 ">
         <h3 class="block py-4 font-bold tracking-wider">EXPERIENCE</h3>
         <div class="flex">
-        <ul class="w-full">
+        <ul v-for="experience in experiences" class="w-full">
             <li class="flex items-center text-sm py-1">
                     <span class="text-xs mr-3">
                     <font-awesome-icon icon="fa-solid fa-caret-right" />
                 </span>
-                PTO COMPUTER SALES AND SERVICES
+                {{experience.campany}}
             </li>
             <li class="flex items-center text-sm py-1 ml-4">                           
-                <p>Computer Technician / IT Support</p>   
+                <p>{{ experience.position }}</p>   
             </li> 
             <li class="flex items-center text-sm py-1 ml-4">                           
-                <p>5 years experience</p>   
+                <p>{{ experience.years }}</p>   
             </li>                    
         </ul>                  
         </div>
     </div>
 </template>
+
+<script setup>
+ const experiences = [
+   {
+     campany : 'PTO Computer Sales and Services',
+     position : 'Computer Technician / IT Support',
+     years: '5 years experience',
+     jobDescription : [],
+   }
+   ]
+  
+</script>
